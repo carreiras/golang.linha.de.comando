0196594db999627c0a61128bb6491e3c4ab8acfb
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-06-20 13:20:59 -0300

    Criando a funcionalidade de Busca por Servidor

--- app/app.go
+++ app/app.go
@@ -10,34 +10,44 @@ import (
 
 /*
 * Gerar cria uma nova aplicação de linha de comando usando o pacote urfave/cli.
-*/
+ */
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de Comando"
 	app.Usage = "Busca IP's e nomes de servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+			Usage: "Especifica o host para buscar o IP",
+		},
+	}
+
 	/*
 	 * @desc   Define o comando principal da aplicação.
 	 *         Este comando é chamado quando o usuário executa a aplicação sem argumentos adicionais.
 	 *
-	 * @usage  go run main.go ip --parametro valor
+	 * @usage  go run main.go ips --parametro valor
 	 *
 	 * @example
-	 *         go run main.go ip (retorna o IP do google.com)
-	 *         go run main.go ip --host amazon.com.br (retorna o IP do amazon.com.br)
+	 *         go run main.go ips (retorna o(s) IP(s) do google.com)
+	 *         go run main.go ips --host amazon.com.br (retorna o(S) IP(s) do amazon.com.br)
+	 *
+	 *         go run main.go servidores (retorna o(s) servidor(s) do google.com)
+	 *         go run main.go servidores --host amazon.com.br (retorna o(s) servidor(es) do amazon.com.br)
 	 */
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-					Usage: "Especifica o host para buscar o IP",
-				},
-			},
+			Name:   "ips",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		}, {
+			Name:   "servidores",
+			Usage:  "Busca nomes de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -49,10 +59,24 @@ func buscarIps(c *cli.Context) {
 	ips, erro := net.LookupIP(host)
 
 	if erro != nil {
+
 		log.Fatal("Erro ao buscar IPs:", erro)
 	}
 
 	for _, ip := range ips {
-		fmt.Println("IP encontrado:", ip)
+		fmt.Println("IP(s) encontrado(s):", ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, erro := net.LookupNS(host) //name server
+
+	if erro != nil {
+		log.Fatal("Erro ao buscar IPs:", erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println("Servidor(es) encontrado:", servidor)
 	}
 }
